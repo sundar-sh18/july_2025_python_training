e0064b0d3faac67b1a25deaf43e2d295ee32133a
sundar-sh18 <sundar.sh18@gmail.com>
2025-07-29 15:25:44 +0530

    flask get data

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 
-app.route('/')
-def hello_world():
-    return 'Hello world'
+app.config['MYSQL_HOST']= '103.25.175.234'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'sundar'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def greet():
+    return(
+'hello world')
+
+@app.route('/about/<name>')
+def about(name):
+    return jsonify({'name': name, 'email': 'sundar@gmail.com'})
+
+@app.route('/getdata')
+def getData():
+    cur = mysql.connection.cursor()
+    cur.execute('select * from user')
+    res = cur.fetchall()
+    cur.close()
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run()
