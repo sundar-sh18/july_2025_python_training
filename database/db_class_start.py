96aa82e12391e08aa078f74daced57ae42b3d5ab
sundar-sh18 <sundar.sh18@gmail.com>
2025-07-31 12:52:39 +0530

    databse

--- database/db_class_start.py
+++
@@ -1,4 +0,0 @@
-from db_class import Database
-
-database = Database()
-database.insert(103,'mayur', 'mayur@gmail.com', '1256235231')
--- database/db_start.py
+++
@@ -1,8 +0,0 @@
-
-from db_methods import insertData,createTable,selectTable,describe,deleteData
-
-# insertData(102, 'Sundar', 'sundar@gmail.com','1294584457')
-selectTable()
-# createTable()
-# describe()
-# deleteData(101)
